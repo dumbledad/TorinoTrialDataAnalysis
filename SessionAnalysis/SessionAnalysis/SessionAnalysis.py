4f9f5342dc0b42e53eeac5ec705a87220a670e38
Tim Regan <timregan@microsoft.com>
2018-04-25 17:29:39 +0100

    Restructured code ready for counting nodes and splitting sessions

--- SessionAnalysis/SessionAnalysis/SessionAnalysis.py
+++ SessionAnalysis/SessionAnalysis/SessionAnalysis.py
@@ -1,6 +1,6 @@
 # Using Python 3.6
 
-import os, io
+import os, io, re
 from functools import reduce
 
 path = "F:\\Users\\timregan\\Microsoft\\ProjectTorino - Logs from Trialists\\"
@@ -18,8 +18,11 @@ for subDir in subDirs:
     # Run through the log files
     for logFileName in [fileName for fileName in loggedFileNames if fileName.lower().startswith("log")]:
 
-        # Start the next line for the CSV file
-        newLine = subDir + ","
+        # Set up counts etc.
+        numPlay = 0
+        numRead = 0
+        dateTime = ""
+        duration = ""
 
         # Open the log file
         with io.open(path + subDir + "\\Logs\\" + logFileName, "r", encoding="latin-1") as file:
@@ -29,29 +32,32 @@ for subDir in subDirs:
 
             if len(fileTextLines) > 0 and fileTextLines[0].startswith("Log started: "):
 
-                # Save the date and time
-                newLine = newLine + fileTextLines[0].replace("Log started: ", "") + ","
-
-                # Count number of plays and reads
-                numPlay = 0
-                numRead = 0
+                # Process each line
                 for fileTextLine in fileTextLines:
+
+                    # Save the date and time
+                    if fileTextLine.startswith("Log started: "):
+                        dateTime = fileTextLine.replace("Log started: ", "")
+
+                    # Increment the counts
                     if "StartPlaying" in fileTextLine:
                         numPlay = numPlay + 1
                     if "ReadCode" in fileTextLine:
                         numRead = numRead + 1
 
-                # Parse out the duration
-                duration = ""
-                for fileTextLine in reversed(fileTextLines):
-                    if "] [" in fileTextLine:
-                        secondsBit = fileTextLine.split("] [")[-1]
-                        duration = secondsBit.split(".")[0]
-                        break
-                newLine = newLine + duration + "," + str(numPlay) + "," + str(numRead)
+                    # Parse out the duration
+                    match = re.search("\] \[([0-9]+)", fileTextLine)
+                    if match != None:
+                        candidateDuration = match.group(1)
+                        if candidateDuration != "":
+                            duration = candidateDuration
 
-                # Add the new line to the CSV
-                csvFileLines.append(newLine)
+                    # TODO: If too much time has passed start a new session
+                    # TODO: Load the coad file and keep maxumum number of nodes and list of node types
+
+        # Add the new line to the CSV
+        if dateTime != "" and duration != "":
+            csvFileLines.append(subDir + "," + dateTime + "," + duration + "," + str(numPlay) + "," + str(numRead))
 
 # Save the CSV to disk
 with io.open(path + "\\Sessions.csv", "w") as outputFile:
