968e7e8508566416d3a6014c1ed029d7b159c087
Tim Regan <timregan@microsoft.com>
2018-04-27 16:16:19 +0100

    Breaking up long sessions
    Untested

--- SessionAnalysis/SessionAnalysis/SessionAnalysis.py
+++ SessionAnalysis/SessionAnalysis/SessionAnalysis.py
@@ -1,10 +1,23 @@
 # Using Python 3.6
 
 import os, io, re
+from datetime import datetime
+from datetime import timedelta
 from functools import reduce
 
-path = "F:\\Users\\timregan\\Microsoft\\ProjectTorino - Logs from Trialists\\"
-csvFileLines = [ "Name,DateTime,Duration,PlayCount,ReadCount" ]
+path = 'E:\\Users\\timregan\\Microsoft\\ProjectTorino - Logs from Trialists\\'
+csvFileLines = [ 'Name,DateTime,Duration,PlayCount,ReadCount' ]
+maxSecondsGapInSession = 3600 # One hour
+
+def GetSession(name):
+    return { 'name' : name, 'start' : '', 'duration' : 0, 'offset' : 0, 'numPlay' : 0, 'numRead' : 0 }
+
+def AddSessionToCSV(session, csvLines):
+    if session['start'] != '' and session['duration'] != '':
+        start = datetime.strptime(session['start'], '%d/%m/%Y %H:%M:%S')
+        start = start + timedelta(seconds=session['offset'])
+        startStr = datetime.strftime(start, '%d/%m/%Y %H:%M:%S')
+        csvLines.append(session['name'] + ',' + startStr + ',' + str(session['duration'] - session['offset']) + ',' + str(session['numPlay']) + ',' + str(session['numRead']))
 
 # Read the sub directories
 subDirs = next(os.walk(path))[1]
@@ -13,53 +26,60 @@ subDirs = next(os.walk(path))[1]
 for subDir in subDirs:
 
     # Read the log, code, and exception files so that we can ingest them
-    loggedFileNames = [fileName for fileName in os.listdir(path + subDir + "\\Logs") if fileName.lower().endswith("txt")]
+    loggedFileNames = [fileName for fileName in os.listdir(path + subDir + '\\Logs') if fileName.lower().endswith('txt')]
 
     # Run through the log files
-    for logFileName in [fileName for fileName in loggedFileNames if fileName.lower().startswith("log")]:
+    for logFileName in [fileName for fileName in loggedFileNames if fileName.lower().startswith('log')]:
 
         # Set up counts etc.
-        numPlay = 0
-        numRead = 0
-        dateTime = ""
-        duration = ""
+        currentSession = GetSession(subDir)
+        previousDuration = 0
 
         # Open the log file
-        with io.open(path + subDir + "\\Logs\\" + logFileName, "r", encoding="latin-1") as file:
+        with io.open(path + subDir + '\\Logs\\' + logFileName, 'r', encoding='latin-1') as file:
 
             # Read the log file and split it into lines
             fileTextLines = file.read().split('\n')
 
-            if len(fileTextLines) > 0 and fileTextLines[0].startswith("Log started: "):
+            if len(fileTextLines) > 0 and fileTextLines[0].startswith('Log started: '):
 
                 # Process each line
                 for fileTextLine in fileTextLines:
 
-                    # Save the date and time
-                    if fileTextLine.startswith("Log started: "):
-                        dateTime = fileTextLine.replace("Log started: ", "")
-
-                    # Increment the counts
-                    if "StartPlaying" in fileTextLine:
-                        numPlay = numPlay + 1
-                    if "ReadCode" in fileTextLine:
-                        numRead = numRead + 1
-
                     # Parse out the duration
-                    match = re.search("\] \[([0-9]+)", fileTextLine)
+                    match = re.search('\] \[([0-9]+)', fileTextLine)
                     if match != None:
                         candidateDuration = match.group(1)
-                        if candidateDuration != "":
-                            duration = candidateDuration
+                        if candidateDuration != '':
+                            durationInt = int(candidateDuration)
+                            if (durationInt - previousDuration) > maxSecondsGapInSession:
+                                # We need to start a new session
+                                AddSessionToCSV(currentSession, csvFileLines)
+                                newSession = GetSession(subDir)
+                                newSession['start'] = currentSession['start']
+                                newSession['offset'] = durationInt
+                                currentSession = newSession
+                            else:
+                                currentSession['duration'] = durationInt
+                            previousDuration = durationInt
 
-                    # TODO: If too much time has passed start a new session
+                    # Save the date and time
+                    if fileTextLine.startswith('Log started: '):
+                        currentSession['start'] = fileTextLine.replace('Log started: ', '')
+
+                    # Increment the counts
+                    if 'StartPlaying' in fileTextLine:
+                        currentSession['numPlay'] = currentSession['numPlay'] + 1
+                    if 'ReadCode' in fileTextLine:
+                        currentSession['numRead'] = currentSession['numRead'] + 1
+
+
                     # TODO: Load the coad file and keep maxumum number of nodes and list of node types
 
-        # Add the new line to the CSV
-        if dateTime != "" and duration != "":
-            csvFileLines.append(subDir + "," + dateTime + "," + duration + "," + str(numPlay) + "," + str(numRead))
+
+
 
 # Save the CSV to disk
-with io.open(path + "\\Sessions.csv", "w") as outputFile:
-    mergedOutputLines = "\n".join(csvFileLines)
+with io.open(path + '\\Sessions.csv', 'w') as outputFile:
+    mergedOutputLines = '\n'.join(csvFileLines)
     outputFile.writelines(mergedOutputLines)
