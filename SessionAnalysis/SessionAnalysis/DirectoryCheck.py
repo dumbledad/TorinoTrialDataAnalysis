17db6be02b747f2e2dfff227d51070bafe6cd83f
Tim Regan <timregan@microsoft.com>
2018-09-06 10:59:13 +0100

    WiP checking duplicate directories

--- SessionAnalysis/SessionAnalysis/DirectoryCheck.py
+++ SessionAnalysis/SessionAnalysis/DirectoryCheck.py
@@ -7,17 +7,28 @@ from functools import reduce
 # Constants
 path = 'C:\\Users\\timregan\\Microsoft\\ProjectTorino - Logs from Trialists\\'
 
-subDirs = next(os.walk(path))[1]
+#sub_dirs = next(os.walk(path))[1]
 
-for subDir in subDirs:
-    subSubDirs = next(os.walk(path + subDir))[1]
-    for subSubDir in subSubDirs:
-        print(path + subDir + '\\' + subSubDir)
-    files = [name for name in os.listdir(path + subDir) if os.path.isfile(path + subDir + '\\' + name)]
-    print(len(files))
+def move_to_log(sub_dirs):
+    files = [name for name in os.listdir(path + sub_dir) if os.path.isfile(path + sub_dir + '\\' + name)]
     if len(files) > 0:
-        os.mkdir(path + subDir + '\\Logs')
-        for fileName in files:
-            existingPath = path + subDir + '\\' + fileName
-            newPath = path + subDir + '\\Logs\\' + fileName
-            os.rename(existingPath, newPath)
+        print(len(files))
+        logs_path = path + sub_dir + '\\Logs'
+        if not os.path.exists(logs_path):
+            os.mkdir(logs_path)
+        for file_name in files:
+            existing_path = path + sub_dir + '\\' + file_name
+            new_path = path + sub_dir + '\\Logs\\' + file_name
+            os.rename(existing_path, new_path)
+
+def print_sub_sub_dirs(sub_dirs):
+    for sub_dir in sub_dirs:
+        sub_sub_dirs = next(os.walk(path + sub_dir))[1]
+        for sub_sub_dir in sub_sub_dirs:
+            print(path + sub_dir + '\\' + sub_sub_dir)
+
+def directory_overlap(sub_dirs):
+    files = {}
+    for sub_dir in sub_dirs:
+        files[sub_dir] = [name for name in os.listdir(path + sub_dir + '\\Logs\\') if os.path.isfile(path + sub_dir + '\\Logs\\' + name)]
+    
